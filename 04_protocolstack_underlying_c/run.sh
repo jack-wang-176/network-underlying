bbeff7aa7c2872ceb54da8986730db79351d4075
jack wang <77239050qq@gamil.com>
2026-03-10 11:13:21 +0800

    添加了第四部分的启动脚本文件

---
+++ 04_protocolstack_underlying_c/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "开始编译..."
+gcc src/*.c -o my_stack
+
+if [ $? -ne 0]; then
+   echo "编译失败"
+   exit 1
+fi
+echo "编译成功，生成可执行文件my_stack"
+echo "启动虚拟网卡"
+
+sudo bash tools/setup_tap.sh
+echo "运行可执行文件"
+sudo ./my_app
+echo "已经推出程序，正在清理网卡"
+sudo bash tools/setup_tap.sh down
+echo "网卡清理完毕"
---
+++ 04_protocolstack_underlying_c/tools/setup_tap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TAP_DEV="tap0"
+TAP_IP="10.0.0.1"
+
+if [ "$1" == "down" ];then
+  sudo ip link delete $TAP_DEV
+  echo "TAP device TAP_DEV removed."
+else
+  sudo ip tuntap add mode tap dev $TAP_DEV
+  sudo ip link set dev $TAP_DEV up
+  sudo ip addr add $TAP_IP/24 dev $TAP_DEV
+  echo "TAP device $TAP_DEV is up with IP $TAP_IP"
+  echo "Pinging an Ip in the same subnet to test it"
+fi
+
